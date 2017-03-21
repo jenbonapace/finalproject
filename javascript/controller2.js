af9d3d305bfe3b46937b9368d148454979908c94
Jennifer Bonapace <jennifer.bonapace@gmail.com>
2017-03-21 12:26:34 -0400

    added photo to bio,twiiter feed style, figuring out googlemapapi

--- javascript/controller2.js
+++ javascript/controller2.js
@@ -1,7 +1,8 @@
-
+var app = angular.module("Mod", ['ngRoute']);
+//
 app.controller("controller2", function(
   $.get({
-url: "https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap",
+url: "https://maps.googleapis.com/maps/api/js?key=AIzaSyBRgU3HN4hiy6vm3cNr0lOZSHIJOH-E1xE&callback=initMap",
 data: {
  name: "Google map",
 api_key: "AIzaSyBRgU3HN4hiy6vm3cNr0lOZSHIJOH-E1xE"
@@ -10,3 +11,27 @@ api_key: "AIzaSyBRgU3HN4hiy6vm3cNr0lOZSHIJOH-E1xE"
 function(responseBody) {
   console.log(responseBody);
  });
+//yelp api
+// app.controller("controller2", function(
+//   $.get({
+//     url:"https://api.yelp.com/v3/businesses/search"
+//     data:{
+//       name:"",
+//       api_key:""
+//       term:
+//       location:
+//       latitude:
+//       longititude:
+//
+//
+//     }}));
+//
+//
+//
+
+
+
+
+
+  })
+))
